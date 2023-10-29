39946dcf800ce6e7d609a61960dabb89a70dd5a5
Tom Griesler <tomgr@umich.edu>
2023-10-29 10:07:21 -0400

    implement fse simulation

--- hw2/problem2.py
+++ hw2/problem2.py
@@ -29,7 +29,7 @@ def create_se_image(t1map, t2map, m0map, te, tr):
 
 def create_fse_images(t1map, t2map, m0map, esp, tr, etl):
     
-    result = np.zeros((np.shape(t2map) + (etl,)))
+    result = np.zeros((np.shape(t2map) + (etl,)), dtype=complex)
 
     for ii in range(np.shape(t1map)[0]):
         for jj in range(np.shape(t1map)[1]):
@@ -59,26 +59,54 @@ def plot_image(result, title, compare, comparetitle):
     plt.tight_layout()
     plt.show()
 
-#%% PD weighted
+#%% PD weighted SE
 te = 15
 tr = 4000
 result = np.abs(create_se_image(t1map, t2map, m0map, te, tr))
 plot_image(result, f'PD weighted\nTE={te}ms, TR={tr}ms', m0map, 'Proton Density')
 
-#%% T1 weighted
+#%% T1 weighted SE
 te = 15
 tr = 500
 result = np.abs(create_se_image(t1map, t2map, m0map, te, tr))
 plot_image(result, f'T1 weighted\nTE={te}ms, TR={tr}ms', t1map, 'T1')
 
-#%%
+#%% T2 weighted SE
 te = 100
 tr = 6000
 result = np.abs(create_se_image(t1map, t2map, m0map, te, tr))
 plot_image(result, f'T2 weighted\nTE={te}ms, TR={tr}ms', t2map, 'T2')
 
-#%%
-images = {teff: {'image': create_se_image(t1map, t2map, m0map, teff, 3000)} for teff in np.arange(5, 165, 5)}
+#%% FSE
+tr = 3000
+esp = 5
+etl = 128
+te_eff = 80
+
+result = create_fse_images(t1map, t2map, m0map, esp, tr, etl)
+
 # %%
-for key in images.keys():
-    images.
+fts = np.zeros_like(result, dtype=complex)
+
+for ii in range(etl):
+    fts[..., ii] = np.fft.fftshift(np.fft.fft2(result[..., ii]))
+
+# %%
+final_kpace = np.zeros_like(t1map, dtype=complex)
+
+index = int((etl/2-te_eff/esp)%etl)
+
+for ii in range(etl):
+
+    print(index)
+
+    k1 = int(index*256/etl)
+    k2 = int((index+1)*256/etl)
+
+    final_kpace[k1:k2] = fts[k1:k2, :, ii]
+
+    index = int((index+1)%etl)
+
+
+plt.imshow(np.abs(np.fft.ifft2(final_kpace)))
+# %%
